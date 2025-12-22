f0f9bd062d622f26eea5491d51037c520fdae05c
Yerrincar <rinconcardosoyeray@gmail.com>
2025-12-22 23:59:53 +0100

    Implemente API Endopoint to serve books

--- api/books.go
+++ api/books.go
@@ -1,12 +1,12 @@
-package main
+package api
 
 import (
 	"archive/zip"
 	"encoding/json"
 	"encoding/xml"
-	"fmt"
 	"io"
 	"log"
+	"net/http"
 	"os"
 	"strings"
 )
@@ -26,39 +26,47 @@ type MetaData struct {
 //	Title string `xml:"http://purl.org/dc/elements/1.1/ title" json:"title"`
 //}
 
-func main() {
-	data, err := os.ReadDir("./../books/")
-	booksJson := make([]MetaData, len(data))
+func ServeJson(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		structs, err := serveStruct()
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		err = json.NewEncoder(w).Encode(structs)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	default:
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+	}
+}
+
+func serveStruct() ([]MetaData, error) {
+	path := "./books/"
+	data, err := os.ReadDir(path)
+	booksJson := make([]MetaData, 0, len(data))
 	if err != nil {
 		log.Fatal("Err while reading the books folder: ", err)
 	}
 	for _, e := range data {
 		if !e.IsDir() && strings.HasSuffix(e.Name(), "epub") {
-			fmt.Print("\nNombre: ", e.Name())
 			bookData, err := extractMetadata(e.Name())
 			if err != nil {
-				log.Fatalf("\nErr extracting data from book: %s | %v", e.Name(), err)
+				log.Printf("\nErr extracting data from book: %s | %v", e.Name(), err)
 			}
 			booksJson = append(booksJson, bookData.Metadata)
 		}
 	}
-	bookDataJson, err := json.Marshal(booksJson)
-	if err != nil {
-		log.Fatalf("\nErr json data: %v", err)
-	}
-	fmt.Println("PRUEBA: ", string(bookDataJson))
-	fmt.Println("Latest Book Title: ", booksJson[len(booksJson)-1].Title)
+	return booksJson, nil
 }
-
-/* Los siguientes problemas que tengo que resolver son:
-- Cambiar la función para devolver el el struct ya que será mejor enviarlo como tal y que el front haga el json.Marshal
-- Luego, cuando le pase los datos en si, indicar que el handlefunc es un json también es importante
-- Arreglar problema de capacidad para que solo pille los .epub o que sea y que aumente según se le añada, tercer parámetro de make....
-*/
 
 func extractMetadata(src string) (data *Package, err error) {
 	var BookData Package
-	path := "./../books/"
+	path := "./books/"
 	r, err := zip.OpenReader(path + src)
 	if err != nil {
 		log.Fatalf("Err opening .epub file: %v", err)
@@ -76,13 +84,10 @@ func extractMetadata(src string) (data *Package, err error) {
 			if err != nil {
 				log.Fatalf("Err trying to read the content of the .opf file: %v", err)
 			}
-			fmt.Println(string(rcBytes))
 			err = xml.Unmarshal(rcBytes, &BookData)
 			if err != nil {
 				log.Fatalf("Err parsing xml data: %v", err)
 			}
-			jsonBytes, _ := json.MarshalIndent(BookData.Metadata, "", " ")
-			fmt.Println("MetaData: ", string(jsonBytes))
 		}
 	}
 	return &BookData, nil
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"Kindria/api"
 	"fmt"
 	"log"
 	"net/http"
@@ -10,6 +11,8 @@ func main() {
 	http.Handle("/", http.FileServer(http.Dir("./web/build")))
 
 	http.Handle("/books/", http.StripPrefix("/books/", http.FileServer(http.Dir("./books"))))
+	http.HandleFunc("/library", api.ServeJson)
+	//API endpoint done, now the fron must fetch the data from it
 
 	fmt.Println("Kindria running on http://localhost:4545")
 	log.Fatal(http.ListenAndServe(":4545", nil))
