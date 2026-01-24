685b97f4d8221363ce1cbd404a54bb263740d498
Yerrincar <rinconcardosoyeray@gmail.com>
2026-01-24 13:04:02 +0100

    Need to improve cover quality

--- go.mod
+++ go.mod
@@ -16,6 +16,7 @@ require (
 	github.com/clipperhouse/displaywidth v0.4.1 // indirect
 	github.com/clipperhouse/stringish v0.1.1 // indirect
 	github.com/clipperhouse/uax29/v2 v2.3.0 // indirect
+	github.com/disintegration/imaging v1.6.2 // indirect
 	github.com/dustin/go-humanize v1.0.1 // indirect
 	github.com/erikgeiser/coninput v0.0.0-20211004153227-1c3628e74d0f // indirect
 	github.com/glebarez/go-sqlite v1.22.0 // indirect
--- internal/tui/model.go
+++ internal/tui/model.go
@@ -10,11 +10,7 @@ import (
 	"github.com/charmbracelet/bubbles/paginator"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
-)
-
-const (
-	cardWith   = 30
-	cardHeight = 25
+	"github.com/disintegration/imaging"
 )
 
 var (
@@ -25,27 +21,25 @@ var (
 
 	list = lipgloss.NewStyle().Border(lipgloss.NormalBorder(), true, true, true, true).
 		BorderForeground(subtle).
-		MarginRight(2).
-		Height(cardHeight).
-		Width(cardWith)
+		MarginRight(1)
 
 	listFocused = lipgloss.NewStyle().Border(lipgloss.NormalBorder(), true, true, true, true).
 			BorderForeground(highlight).
-			MarginRight(2).
-			Height(cardHeight).
-			Width(cardWith)
+			MarginRight(1)
 )
 
 type Model struct {
-	books     []*metadata.Package
-	cursor    int
-	selected  map[int]struct{}
-	width     int
-	height    int
-	cols      int
-	paginator paginator.Model
-	covers    map[int]string
-	handler   metadata.Handler
+	books             []*metadata.Package
+	cursor            int
+	selected          map[int]struct{}
+	width             int
+	height            int
+	dynamicCardWidth  int
+	dynamicCardHeight int
+	cols              int
+	paginator         paginator.Model
+	covers            map[int]string
+	handler           metadata.Handler
 }
 
 type coversLoadedMsg map[int]string
@@ -88,12 +82,15 @@ func (m *Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	case tea.WindowSizeMsg:
 		debugLog("WindowSize: w=%d h=%d", msg.Width, msg.Height)
 		m.width, m.height = msg.Width, msg.Height
-		m.cols = m.width / cardWith
-		if m.cols < 1 {
-			m.cols = 1
+		m.cols = 6
+		m.dynamicCardWidth = (m.width / m.cols) - 3
+		m.dynamicCardHeight = int(float64(m.dynamicCardWidth) * 0.66)
+		if m.dynamicCardWidth < 10 {
+			m.cols = 2 // Fallback if too small
+			m.dynamicCardWidth = (m.width / 2) - 3
+			m.dynamicCardHeight = int(float64(m.dynamicCardWidth) * 1.0)
 		}
-
-		m.paginator.PerPage = m.cols * (m.height / cardHeight)
+		m.paginator.PerPage = m.cols * (m.height / (m.dynamicCardHeight + 2))
 		m.paginator.SetTotalPages(len(m.books))
 		cmdSync = m.syncVisibleWidget()
 
@@ -133,86 +130,96 @@ func (m *Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 
 func (m Model) View() string {
 	debugLog("View")
+	if m.cols <= 0 || m.dynamicCardWidth <= 0 {
+		return "\n  Initializing Kindria..."
+	}
 	var b strings.Builder
 	b.WriteString("\n Kindria, your TUI e-book library\n")
 
 	start, end := m.paginator.GetSliceBounds(len(m.books))
 	booksCards := make([]string, 0)
 
-	for i, book := range m.books[start:end] {
+	for i, _ := range m.books[start:end] {
 		absoluteIndex := i + start
 		cover, ok := m.covers[absoluteIndex]
 		if !ok || cover == "" {
 			cover += strings.Repeat("\n", 10)
 		}
-		renderedBook := m.renderBookCard(book, absoluteIndex == m.cursor, cover)
-		booksCards = append(booksCards, renderedBook)
-	}
+		style := list.Copy().
+			Width(m.dynamicCardWidth).
+			Height(m.dynamicCardHeight)
 
-	finalRows := make([]string, 0)
-	if m.cols < 1 {
-		m.cols = 1
-	}
-	for i := 0; i < len(booksCards); i += m.cols {
-		endRows := i + m.cols
-		if endRows > len(booksCards) {
-			endRows = len(booksCards)
+		if absoluteIndex == m.cursor {
+			style = style.BorderForeground(highlight)
 		}
-
-		rowSlice := booksCards[i:endRows]
-		finalRows = append(finalRows, lipgloss.JoinHorizontal(lipgloss.Top, rowSlice...))
+		//renderedBook := m.renderBookCard(book, absoluteIndex == m.cursor, cover)
+		booksCards = append(booksCards, style.Render(cover))
 	}
-	finalRowStacked := lipgloss.JoinVertical(lipgloss.Top, finalRows...)
-	b.WriteString(finalRowStacked)
-	b.WriteString("\n  " + m.paginator.View())
-	b.WriteString("\n\n  h/l ←/→ page • q: quit\n")
 
+	var rows []string
+	for i := 0; i < len(booksCards); i += m.cols {
+		endIdx := i + m.cols
+		if endIdx > len(booksCards) {
+			endIdx = len(booksCards)
+		}
+		rows = append(rows, lipgloss.JoinHorizontal(lipgloss.Top, booksCards[i:endIdx]...))
+	}
+
+	b.WriteString(lipgloss.JoinVertical(lipgloss.Left, rows...))
+	b.WriteString("\n  " + m.paginator.View())
 	return b.String()
 }
 
-func (m Model) renderBookCard(p *metadata.Package, isFocused bool, imageString string) string {
-	lipglossStyle := list
-	if isFocused {
-		lipglossStyle = listFocused
-	}
+//func (m Model) renderBookCard(p *metadata.Package, isFocused bool, imageString string) string {
+//	lipglossStyle := list
+//	if isFocused {
+//		lipglossStyle = listFocused
+//	}
+//
+//	//line11 := truncString(p.Metadata.Title, 18)
+//	//line12 := truncString(p.Metadata.Author, 18)
+//
+//	finalCard := lipglossStyle.Render(lipgloss.JoinVertical(lipgloss.Left, imageString))
+//	return finalCard
+//}
 
-	line11 := truncString(p.Metadata.Title, 18)
-	line12 := truncString(p.Metadata.Author, 18)
-
-	finalCard := lipglossStyle.Render(lipgloss.JoinVertical(lipgloss.Left, imageString, line11, line12))
-	return finalCard
-}
-
-func truncString(s string, max int) string {
-	if len(s) > max {
-		return s[:max]
-	}
-	return s
-}
+//func truncString(s string, max int) string {
+//	if len(s) > max {
+//		return s[:max]
+//	}
+//	return s
+//}
 
 func (m *Model) syncVisibleWidget() tea.Cmd {
 	start, end := m.paginator.GetSliceBounds(len(m.books))
 	localCovers := make(map[int]string)
 	booksToLoad := m.books[start:end]
+	curWidth := m.dynamicCardWidth
+	curHeight := m.dynamicCardHeight
 	debugLog("syncVisibleWidget queued: start=%d end=%d books=%d", start, end, len(booksToLoad))
 	return func() tea.Msg {
 		debugLog("syncVisibleWidget start: start=%d end=%d books=%d", start, end, len(booksToLoad))
-		for i, book := range booksToLoad {
-			path, err := m.handler.SelectBookPath(book.BookFile)
+		for i, _ := range booksToLoad {
+			//	path, err := m.handler.SelectBookPath(book.BookFile)
+			//	if err != nil {
+			//		log.Printf("Error getting book path for book: "+book.BookFile+"%v: ", err)
+			//		continue
+			//	}
+			targetPixelWidth := curWidth * 48
+			targetPixelHeight := curHeight * 72
+			srcImage, err := imaging.Open("./cache/covers/Reyes_de_la_Tierra_Salvaje.jpg")
 			if err != nil {
-				log.Printf("Error getting book path for book: "+book.BookFile+"%v: ", err)
 				continue
 			}
-			cover, err := termimg.NewImageWidgetFromFile(path)
-			if err != nil {
-				log.Printf("Error trying to get cover from file: %v ", err)
-				continue
-			}
-			cover.SetSize(30, 20).SetProtocol(termimg.Kitty)
+			resizedImage := imaging.Fit(srcImage, targetPixelWidth, targetPixelHeight, imaging.CatmullRom)
+			resizedImage = imaging.Sharpen(resizedImage, 1.5)
+			cover := termimg.NewImageWidgetFromImage(resizedImage)
 
+			cover.SetSize(curWidth, curHeight).SetProtocol(termimg.Auto)
 			finalCover := ""
 			if cover != nil {
 				coverRendered, err := cover.Render()
+				coverRendered = lipgloss.NewStyle().Width(curWidth).Height(curHeight).Render(coverRendered)
 				if err != nil {
 					log.Printf("Err rendering cover: %v ", err)
 				}
